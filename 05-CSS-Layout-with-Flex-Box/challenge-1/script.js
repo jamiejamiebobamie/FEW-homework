f0f1ee3c8cac4d2f680acb881f08ba2dbd63f5b1
jamiejamiebobamie <jmccrory@vt.edu>
2018-12-01 17:10:54 -0800

    made the tip calculator functional: FEW/05/challeneg-1

--- 05-CSS-Layout-with-Flex-Box/challenge-1/script.js
+++ 05-CSS-Layout-with-Flex-Box/challenge-1/script.js
@@ -2,21 +2,19 @@ let amount = document.getElementById('amount')
 let percent = document.getElementById('percent')
 let calculate = document.getElementById('calculate');
 let tip = document.getElementById('tip')
-let total = document.getElementById('percent')
+let total = document.getElementById('total')
 
-
-function calc() {
-    tip = amount * Math.floor(percent/100);
-    total = amount + tip;
-};
-
-function update() {
-    tip.innerHTML = tip.toString;
-    total.innerHTML = total.toString;
+function calcAndUpdate() {
+    // tip.innerHTML = amount * Math.floor(percent/100);
+    // total.innerHTML = amount + Math.floor(percent/100);
+    let tipz = (amount.value * (percent.value/100));
+    tip.innerHTML = Number(tipz);
+    // tip.innerHTML = Math.floor(amount.value * (percent.value/100))
+    totalz = Number(amount.value) + Number(tipz);
+    total.innerHTML = totalz;
 };
 
 
 calculate.addEventListener("click", function() {
-    calc();
-    update();
+    calcAndUpdate();
 });
